abad54ac53e2d3a11b7723f9d8806d5952630d1e
unknown <pavlobutynez@gmail.com>
2025-01-02 12:23:46 +0200

    replaced attachment with attachments in down function

--- migrations/20241222143712-add-resource-type-delete-availability-in-attachments.js
+++ migrations/20241222143712-add-resource-type-delete-availability-in-attachments.js
@@ -25,7 +25,7 @@ module.exports = {
     )
     await attachmentsCollection.updateMany(
       { resourceType: { $exists: true } },
-      { $unset: { resourceType: 'attachment' } }
+      { $unset: { resourceType: 'attachments' } }
     )
   }
 }
