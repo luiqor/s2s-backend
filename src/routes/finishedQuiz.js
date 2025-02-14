2d0d7d0be37bd0faf85dd505965e52e33c3407ff
marek777-lol <markgol777@gmail.com>
2025-02-14 14:44:30 +0100

    Removed comments

--- src/routes/finishedQuiz.js
+++ src/routes/finishedQuiz.js
@@ -6,16 +6,12 @@ const asyncWrapper = require('~/middlewares/asyncWrapper')
 const isEntityValid = require('~/middlewares/entityValidation')
 const finishedQuizController = require('~/controllers/finishedQuiz')
 const Quiz = require('~/models/quiz')
-// const FinishedQuiz = require('~/models/finishedQuiz')
 const idValidation = require('~/middlewares/idValidation')
-// const { ownerFields } = require('~/consts/auth')
 
 const body = [{ model: Quiz, idName: 'quiz' }]
-// const params = [{ model: FinishedQuiz, idName: 'id' }]
 
 router.use(authMiddleware)
 router.param('id', idValidation)
-// router.use('/by-quiz/:id', isEntityValid({ params }), asyncWrapper(ownershipMiddleware(FinishedQuiz, ownerFields)))
 
 router.get('/', asyncWrapper(finishedQuizController.getFinishedQuizzes))
 router.get('/:id', asyncWrapper(finishedQuizController.getFinishedQuizById))
