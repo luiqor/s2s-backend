1bb2aae87cad126b2f908d5323a31330912f2b33
luiqor <vohnenna@gmail.com>
2025-03-30 18:20:22 +0300

    add migration down test

--- src/test/migrations/20250224153425-rename-resource-completion-status.spec.js
+++ src/test/migrations/20250224153425-rename-resource-completion-status.spec.js
@@ -21,6 +21,16 @@ const cooperationWithOutdatedResources = [
   }
 ]
 
+const cooperationWithUpdatedResources = [
+  {
+    sections: [
+      {
+        resources: [{ completionStatus: 'in progress' }, { completionStatus: 'completed' }]
+      }
+    ]
+  }
+]
+
 describe('20250224153425-rename-resource-completion-status', () => {
   let server
   let db
@@ -58,6 +68,29 @@ describe('20250224153425-rename-resource-completion-status', () => {
     ).length
 
     expect(inProgressCount).toBe(3)
+    expect(completedCount).toBe(1)
+  })
+
+  test('should revert completionStatus to "active" for resources with "in progress" during down migration', async () => {
+    const collection = db.collection(COLLECTION_NAME)
+
+    await collection.insertMany(cooperationWithUpdatedResources)
+
+    await migration.down(db)
+
+    const updatedDocuments = await collection.find().toArray()
+
+    const activeCount = updatedDocuments.flatMap((doc) =>
+      doc.sections.flatMap((section) => section.resources.filter((resource) => resource.completionStatus === 'active'))
+    ).length
+
+    const completedCount = updatedDocuments.flatMap((doc) =>
+      doc.sections.flatMap((section) =>
+        section.resources.filter((resource) => resource.completionStatus === 'completed')
+      )
+    ).length
+
+    expect(activeCount).toBe(1)
     expect(completedCount).toBe(1)
   })
 })
