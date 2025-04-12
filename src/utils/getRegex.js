7c9679fe63a4973acefed44d4dba67d231079f3f
Diana Vashchilina <vohnenna@gmail.com>
2025-04-12 17:26:37 +0300

    Fix resources search trailing backslashes

--- src/utils/getRegex.js
+++ src/utils/getRegex.js
@@ -1,6 +1,20 @@
-const getRegex = (regex = '') => ({
-  $regex: regex.length > 0 ? regex : '.*',
-  $options: 'i'
-})
+const getRegex = (regex = '') => {
+  if (!regex) {
+    return {
+      $regex: '.*',
+      $options: 'i'
+    }
+  }
+
+  let processedRegex = regex
+  if (regex.endsWith('\\')) {
+    processedRegex = regex + '\\'
+  }
+
+  return {
+    $regex: processedRegex,
+    $options: 'i'
+  }
+}
 
 module.exports = getRegex
